14225231c9de6bb07582c7a0ec13e4db04975b28
KaydenDaKat <gippy713@gmail.com>
2021-02-25 13:07:22 -0800

    Updated options menu to take user to other fragments, but some still don't. All the fragments have a options menu now
    Most of the Java classes are converted to Kotlin classes

---
+++ app/src/main/java/com/example/to_brary/HomeFragment.kt
@@ -0,0 +1,83 @@
+package com.example.to_brary
+
+import android.os.Bundle
+import android.view.*
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.GridView
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var mParam1: String? = null
+    private var mParam2: String? = null
+    private var displayGridView: GridView? = null
+    private var customAdapter: GridViewAdapter? = null
+    private val images = intArrayOf(R.drawable.test_image, R.drawable.test_image2) // tester array
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            mParam1 = requireArguments().getString(ARG_PARAM1)
+            mParam2 = requireArguments().getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Inflate the layout for this fragment
+        //   return inflater.inflate(R.layout.fragment_home,null);
+        displayGridView = rootView.findViewById<View>(R.id.gridview_display_home) as GridView
+        customAdapter = GridViewAdapter(images, activity)
+        displayGridView!!.adapter = customAdapter
+        displayGridView!!.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            requireActivity().fragmentManager.popBackStack()
+            Navigation.findNavController(requireActivity(), R.id.fragment_navhost_main).navigate(R.id.action_homeFragment_to_imageViewFragment)
+        }
+        setHasOptionsMenu(true)
+        return rootView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String?, param2: String?): HomeFragment {
+            val fragment = HomeFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
---
+++ app/src/main/java/com/example/to_brary/LoginPage.kt
@@ -0,0 +1,67 @@
+package com.example.to_brary
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginPage.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginPage : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var mParam1: String? = null
+    private var mParam2: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            mParam1 = requireArguments().getString(ARG_PARAM1)
+            mParam2 =requireArguments().getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_login_page, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LoginPage.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String?, param2: String?): LoginPage {
+            val fragment = LoginPage()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
