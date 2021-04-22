47674ee6694906087ff8c4806c3d6d38d0a1cc44
Kimberly K <kimberly.n.kubo@gmail.com>
2021-04-22 13:04:55 -0700

    changed colors. options menu icon is white. Ato-Brary appears on all fragments instead of the fragment name.

--- app/src/main/java/com/example/to_brary/MainActivity.kt
+++ app/src/main/java/com/example/to_brary/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.to_brary
 
 import android.app.SearchManager
 import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.SearchView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
@@ -21,6 +25,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Āto-Brary"
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.title = "Āto-Brary"
+        toolbar.setTitleTextColor(Color.WHITE)
+        toolbar.overflowIcon?.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP)
+        setSupportActionBar(toolbar)
 
         val navController = this.findNavController(R.id.fragment_navhost_main)
         NavigationUI.setupActionBarWithNavController(this, navController)
