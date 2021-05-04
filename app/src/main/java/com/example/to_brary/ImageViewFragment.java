5e36f24a423b2b81c20f266e87253693faed29c0
KaydenDaKat <gippy713@gmail.com>
2021-05-04 12:24:28 -0700

    Tags displayed in the TextViews of the ImageViewFragment are now displayed properly without the json format

--- app/src/main/java/com/example/to_brary/ImageViewFragment.java
+++ app/src/main/java/com/example/to_brary/ImageViewFragment.java
@@ -26,6 +26,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
@@ -34,7 +35,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static com.backendless.rt.RTTypes.log;
@@ -113,10 +117,10 @@ public class ImageViewFragment extends Fragment {
         image = gson.fromJson(json, Image.class);
 
         Picasso.get().load(image.getImageFile()).into(imageView);
-        artistsTextView.setText(image.getArtists());
-        copyrightTextView.setText(image.getCopyright());
-        charactersTextView.setText(image.getCharacters());
-        descriptionsTextView.setText(image.getDetails());
+        artistsTextView.setText(convertGsonToNormalString(image.getArtists()));
+        copyrightTextView.setText(convertGsonToNormalString(image.getCopyright()));
+        charactersTextView.setText(convertGsonToNormalString(image.getCharacters()));
+        descriptionsTextView.setText(convertGsonToNormalString(image.getDetails()));
 
         return rootView;
     }
@@ -137,6 +141,24 @@ public class ImageViewFragment extends Fragment {
                 }
             }
         });
+    }
+
+    public String convertGsonToNormalString(String json)
+    {
+        String tags = "";
+
+        Type collectionType = new TypeToken<ArrayList<String>>(){}.getType();
+        ArrayList<String> tagList = new Gson().fromJson(json, collectionType);
+
+        tags += tagList.get(0);
+
+        if(!(tagList.size() == 1)) {
+            for (int i = 1; i < tagList.size(); i++) {
+                    tags += " " + tagList.get(i);
+                }
+        }
+
+        return tags;
     }
 
     @NonNull
